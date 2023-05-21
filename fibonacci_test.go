a42f735402b18ca5f1fb7fad6572b79d3abbfca1
Sohbetbackend <sgurbanov888@gmail.com>
2023-05-21 12:41:17 +0500

    learn fibonacci in go

---
+++ fibonacci_test.go
@@ -0,0 +1,27 @@
+package main
+
+import "testing"
+
+func TestFibOne(t *testing.T) {
+	actual := fibOne(8)
+
+	if actual != 21 {
+		t.Error("expected value of 8 at position 6")
+	}
+}
+
+func TestFibTwo(t *testing.T) {
+	actual := fibTwo(6, nil)
+
+	if actual != 8 {
+		t.Error("expected value of 8 at position 6")
+	}
+}
+
+func TestFibThree(t *testing.T) {
+	actual := fibThree(6)
+
+	if actual != 8 {
+		t.Error("expected value of 8 at position 6")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Sohbetbackend/go_fibonacci
+
+go 1.20
